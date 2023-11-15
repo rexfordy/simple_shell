cc398141349d185a7e587cf0511424239c12a162
Linus Atte <linuseatte@gmail.com>
2023-11-15 04:04:55 -0800

    Update

--- main.c
+++ main.c
@@ -12,10 +12,12 @@ int main(int ac, char **av)
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=x" (fd)
-		: "x" (fd));
+asm volatile (
+	"mov %[fd], %[result]\n\t"
+	"add $3, %[result]"
+	: [result] "=r" (fd)
+	: [fd] "r" (fd)
+);
 
 	if (ac == 2)
 	{
